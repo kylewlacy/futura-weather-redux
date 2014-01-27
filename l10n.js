f7699479ad340d025ed24216c1864e86252e937a
Kyle Lacy <kylewlacy@me.com>
2014-01-27 15:41:20 -0800

    Move localization data into `l10n.js`
    Updates to languages will be carried across different versions

---
+++ l10n.js
@@ -0,0 +1,42 @@
+var translations = {
+	"0": {
+		"name":        "English",
+		"translation": "Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec,Mon,Tue,Wed,Thu,Fri,Sat,Sun"
+	},
+	"1": {
+		"name":        "Swedish",
+		"translation": "Jan,Feb,Mar,Apr,Maj,Jun,Jul,Aug,Sep,Okt,Nov,Dec,Mån,Tis,Ons,Tor,Fre,Lör,Sön"
+	},
+	"2": {
+		"name":        "Danish",
+		"translation": "Jan,Feb,Mar,Apr,Maj,Jun,Jul,Aug,Sep,Okt,Nov,Dec,Man,Tir,Ons,Tor,Fre,Lør,Søn"
+	},
+	"3": {
+		"name":        "Dutch",
+		"translation": "Jan,Feb,Mrt,Apr,Mei,Jun,Jul,Aug,Sep,Okt,Nov,Dec,Ma,Di,Wo,Do,Vr,Za,Zo"
+	},
+	"4": {
+		"name":        "French",
+		"translation": "Janv,Févr,Mars,Avril,Mai,Juin,Juil,Août,Sept,Oct,Nov,Déc,Lun,Mar,Mer,Jeu,Ven,Sam,Dim"
+	},
+	"5": {
+		"name":        "Finnish",
+		"translation": "Tammi,Helmi,Maalis,Huhti,Touko,Kesä,Heinä,Elo,Syys,Loka,Marras,Joulu,Ma,Ti,Ke,To,Pe,La,Su"
+	},
+	"6": {
+		"name":        "German",
+		"translation": "Jan,Feb,Mär,Apr,Mai,Jun,Jul,Aug,Sep,Okt,Nov,Dez,Mo,Di,Mi,Do,Fr,Sa,So"
+	},
+	"7": {
+		"name":        "Italian",
+		"translation": "Gen,Feb,Mar,Apr,Mag,Giu,Lug,Ago,Set,Ott,Nov,Dic,Lun,Mar,Mer,Gio,Ven,Sab,Dom"
+	},
+	"8": {
+		"name":        "Spanish",
+		"translation": "Ene,Feb,Mar,Abr,May,Jun,Jul,Ago,Sep,Oct,Nov,Dic,Lun,Mar,Mié,Jue,Vie,Sáb,Dom"
+	}
+};
+
+addLocalizations({
+	"translations": translations
+});
