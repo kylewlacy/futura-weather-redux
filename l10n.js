db9e1eead5c6907bd356b350197177c242e39d25
Mattias Eriksson <snaggen@mayam.com>
2014-02-02 00:46:23 +0100

    Add l10n

---
+++ l10n.js
@@ -0,0 +1,78 @@
+var translations = {
+	"0": {
+		"name": "English",
+		"translation": [
+			"Jan", "Feb", "Mar", "Apr", "May", "Jun",
+			"Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
+			"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"
+		]
+	},
+	"1": {
+		"name": "Svenska (Swedish)",
+		"translation": [
+			"Jan", "Feb", "Mar", "Apr", "Maj", "Jun",
+			"Jul", "Aug", "Sep", "Okt", "Nov", "Dec",
+			"Mån", "Tis", "Ons", "Tor", "Fre", "Lör", "Sön"
+		]
+	},
+	"2": {
+		"name": "Dansk (Danish)",
+		"translation": [
+			"Jan", "Feb", "Mar", "Apr", "Maj", "Jun",
+			"Jul", "Aug", "Sep", "Okt", "Nov", "Dec",
+			"Man", "Tir", "Ons", "Tor", "Fre", "Lør", "Søn"
+		]
+	},
+	"3": {
+		"name": "Nederlands (Dutch)",
+		"translation": [
+			"Jan", "Feb", "Mrt", "Apr", "Mei", "Jun",
+			"Jul", "Aug", "Sep", "Okt", "Nov", "Dec",
+			"Ma",  "Di",  "Wo",  "Do",  "Vr",  "Za",  "Zo"
+		]
+	},
+	"4": {
+		"name": "Français (French)",
+		"translation": [
+			"Janv", "Févr", "Mars", "Avril", "Mai", "Juin",
+			"Juil", "Août", "Sept", "Oct",   "Nov", "Déc",
+			"Lun",  "Mar",  "Mer",  "Jeu",   "Ven", "Sam", "Dim"
+		]
+	},
+	"5": {
+		"name": "Suomi (Finnish)",
+		"translation": [
+			"Tammi", "Helmi", "Maalis", "Huhti", "Touko",  "Kesä",
+			"Heinä", "Elo",   "Syys",   "Loka",  "Marras", "Joulu",
+			"Ma",    "Ti",    "Ke",     "To",    "Pe",     "La",    "Su"
+		]
+	},
+	"6": {
+		"name": "Deutsch (German)",
+		"translation": [
+			"Jan", "Feb", "Mär", "Apr", "Mai", "Jun",
+			"Jul", "Aug", "Sep", "Okt", "Nov", "Dez",
+			"Mo",  "Di",  "Mi",  "Do",  "Fr",  "Sa",  "So"
+		]
+	},
+	"7": {
+		"name": "Italiano (Italian)",
+		"translation": [
+			"Gen", "Feb", "Mar", "Apr", "Mag", "Giu",
+			"Lug", "Ago", "Set", "Ott", "Nov", "Dic",
+			"Lun", "Mar", "Mer", "Gio", "Ven", "Sab", "Dom"
+		]
+	},
+	"8": {
+		"name": "Español (Spanish)",
+		"translation": [
+			"Ene", "Feb", "Mar", "Abr", "May", "Jun",
+			"Jul", "Ago", "Sep", "Oct", "Nov", "Dic",
+			"Lun", "Mar", "Mié", "Jue", "Vie", "Sáb", "Dom"
+		]
+	}
+};
+
+addLocalizations({
+	"translations": translations
+});
